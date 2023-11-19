b2415ed0b38cabd254f362ae7bf5a7eaa9eae798
Andrea Piña <123607797+imAndreaa@users.noreply.github.com>
2023-11-19 01:59:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #800961;'>|</span>",
+ cursor: "<span style='color: #d198c3;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #800961;">Estudiante de Tecnología de Software en FIME .</span>')
+  .typeString('<span style="color: #d198c3;">Estudiante de Tecnología de Software en FIME .</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
